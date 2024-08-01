7911d02b1556728dc0430b78f75aca255a70df3e
Miguel Álvarez <128592227+malvads@users.noreply.github.com>
2024-08-01 16:14:49 +0100

    Update default.rb

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -1,4 +1,4 @@
 # default attributes
 #
 default['chrony']['user'] = 'chrony'
-default['chrony']['ntp_servers'] = ['2.rocky.pool.ntp.org']
+default['chrony']['ntp_servers'] = ['time-a.nist.gov', 'time.nist.gov', 'time-b.nist.gov']
